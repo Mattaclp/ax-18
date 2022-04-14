17e0dbd115e8a059f2b011ce7049621f60558c8a
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2022-04-15 07:27:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+rm -rf feeds/kenzo/luci-app-ssr-plus
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
